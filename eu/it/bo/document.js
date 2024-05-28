76971dab806f0bcb8d39ad3073e7dbb56d161147
P5music <4376344+P5music@users.noreply.github.com>
2024-05-28 23:47:42 +0200

    Update document.js

--- eu/it/bo/document.js
+++ eu/it/bo/document.js
@@ -1,4 +1,4 @@
-document.write(`<div class="prodotto-item">
+<div class="prodotto-item">
   <h2>Angurie</h2>
   <p>Frutti biologici, peso variabile da 2 a 5 Kg</p>
 <img height=100 src="https://www.myqueries.info/demo/bbb_borgagne/images/angurie.jpg" alt="Foto del prodotto">
@@ -27,4 +27,4 @@ document.write(`<div class="prodotto-item">
   <h2>Friselle</h2>
   <p>Varietà: normali e integrali</p>
 <img height=100 src="https://www.myqueries.info/demo/bbb_borgagne/images/friselle.jpg" alt="Foto del prodotto">
-</div>`);
+</div>
